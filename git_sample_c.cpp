1ec05bcd834f333defbb8edc53b04e45b5b774bd
ParthaB <pbp2005@gmail.com>
2016-11-29 00:23:52 -0500

    First_commit_from_linux

---
+++ git_sample_c.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+//#include <algorithm>
+using namespace std;
+
+
+void reverseStr(string& x)
+{
+	string temp = " ";
+	int len = x.length();
+
+	for(int i = len-1; i >= 0; --i)
+	{
+		temp += x[i];
+	}
+	x = temp;
+}
+
+int main()
+{
+	string s = "This is a sample program to upload into git from my new ubuntu";
+	cout << "Original string: " << s << endl;
+	
+	//reverse(s.begin(), s.end());
+	reverseStr(s);
+	cout << "Reversed string: " << s << endl;
+
+	return 0;
+}
